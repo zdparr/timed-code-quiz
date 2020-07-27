00fd6a5ed13243872117b2cb5d91bfeabebd1e90
Zach Parr <zachery.parr@gmail.com>
2020-07-27 11:46:43 -0500

    adding reset button

--- assets/scripts/highscore.js
+++ assets/scripts/highscore.js
@@ -1,8 +1,15 @@
 const highScoreList = document.querySelector("#highScoreList");
 const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+const resetBtn = document.querySelector("#resetBTN");
 
 highScoreList.innerHTML = highScores
   .map((score) => {
     return `<p class="styledText">${score.name} - ${score.score}</p>`;
   })
   .join("");
+
+function resetScores() {
+  console.log("reset button clicked");
+}
+
+resetBtn.addEventListener("click", resetScores);
