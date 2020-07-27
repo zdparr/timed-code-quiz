16bbdfd045ea070a0391dbe98c6f04cda1f6792e
Zach Parr <zachery.parr@gmail.com>
2020-07-27 12:00:40 -0500

    selecting reset button event listener

--- assets/scripts/highscore.js
+++ assets/scripts/highscore.js
@@ -1,6 +1,6 @@
 const highScoreList = document.querySelector("#highScoreList");
 const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
-const resetBtn = document.querySelector("#resetBTN");
+const resetBtn = document.querySelector("#resetBtn");
 
 highScoreList.innerHTML = highScores
   .map((score) => {
