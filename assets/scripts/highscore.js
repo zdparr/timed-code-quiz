ad2abc00e2c492e1f617260e0ae3a2b19ad3ffb3
Zach Parr <zachery.parr@gmail.com>
2020-07-27 00:46:26 -0500

    updating highscore js

--- assets/scripts/highscore.js
+++ assets/scripts/highscore.js
@@ -1,4 +1,8 @@
 const highScoreList = document.querySelector("#highScoreList");
 const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
 
-console.log(highScores);
+highScoreList.innerHTML = highScores
+  .map((score) => {
+    return `<li>${score.name} - ${score.score}</li>`;
+  })
+  .join("");
